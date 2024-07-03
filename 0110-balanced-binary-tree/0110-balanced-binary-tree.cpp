2488d33f7ad63fa71d27596fe18f7bd87a7d8c3f
Adwait Prashant Borate <136005137+Adwait-Borate@users.noreply.github.com>
2024-07-03 19:09:19 +0530

    Time: 3 ms (97.95%), Space: 21.6 MB (75.64%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -1,23 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
+    int height(TreeNode* root, int &valid) {
+        if (root == nullptr) {
+            return 0;
+        }
+        int L = height(root->left, valid);
+        int R = height(root->right, valid);
+        if (abs(L - R) > 1) {
+            valid = 0;
+        }
+        return 1 + max(L, R);
+    }
+
     bool isBalanced(TreeNode* root) {
-        // If the tree is empty, we can say it’s balanced...
-        if (root == NULL)  return true;
-        // Height Function will return -1, when it’s an unbalanced tree...
-		if (Height(root) == -1)  return false;
-		return true;
-	}
-    // Create a function to return the “height” of a current subtree using recursion...
-	int Height(TreeNode* root) {
-        // Base case...
-		if (root == NULL)  return 0;
-        // Height of left subtree...
-		int leftHeight = Height(root->left);
-        // Height of height subtree...
-		int rightHight = Height(root->right);
-        // In case of left subtree or right subtree unbalanced or their heights differ by more than ‘1’, return -1...
-		if (leftHeight == -1 || rightHight == -1 || abs(leftHeight - rightHight) > 1)  return -1;
-        // Otherwise, return the height of this subtree as max(leftHeight, rightHight) + 1...
-		return max(leftHeight, rightHight) + 1;
+        int valid = 1;
+        height(root, valid);
+        return valid;
     }
 };
