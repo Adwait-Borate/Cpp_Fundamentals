fd1cd6a64ff72d1e36f43f6e0eb893922cae75bb
Adwait Prashant Borate <136005137+Adwait-Borate@users.noreply.github.com>
2024-07-12 09:36:03 +0530

    Time: 7 ms (50.84%), Space: 13.8 MB (9.92%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,21 @@
+
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> umap;
+        int ans;
+        
+        for(auto &i:nums) {
+            umap[i]++;
+        }
+
+        for(auto &it:umap) {
+            if(it.second!=3) {
+                ans=it.first;
+                break;
+            }
+        }
+        return ans;
+    }
+};
