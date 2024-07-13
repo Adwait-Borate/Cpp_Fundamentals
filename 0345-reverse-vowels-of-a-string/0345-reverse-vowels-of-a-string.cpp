e7a103efd21668615f95ba1680b97945712922bc
Adwait Prashant Borate <136005137+Adwait-Borate@users.noreply.github.com>
2024-07-13 19:49:24 +0530

    Time: 9 ms (36.61%), Space: 10.7 MB (5.73%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,53 +1,53 @@
-// class Solution {
-// public:
-//     string reverseVowels(string s) {
-//         unordered_set<char>uset;
-//         uset.insert("a");uset.insert("e");uset.insert("i");uset.insert("o");
-//         uset.insert("u");uset.insert("A");uset.insert("E");uset.insert("I");
-//         uset.insert("O");uset.insert("U");
-        
-//         int n=s.length();
-//         int i=0;
-//         int j=n-1;
-
-//         while(i<=j){
-//             if(uset.count(s[i]) && uset.count(s[j])){
-//                swap(s[i],s[j]);
-//             }else{
-//                 if(!uset.count(s[i]) && uset.count(s[j])){
-//                     i++;
-//                 }else{
-//                     j--;
-//                 }
-//             }
-//         }
-//         return s;
-//     }
-// };
-
-
 class Solution {
 public:
     string reverseVowels(string s) {
         unordered_set<char> uset;
-        uset.insert('a'); uset.insert('e'); uset.insert('i'); uset.insert('o');
-        uset.insert('u'); uset.insert('A'); uset.insert('E'); uset.insert('I');
-        uset.insert('O'); uset.insert('U');
+            uset.insert('a');uset.insert('e');uset.insert('i');uset.insert('o');
+        uset.insert('u');uset.insert('A');uset.insert('E');uset.insert('I');
+        uset.insert('O');uset.insert('U');
+        
+        int n=s.length();
+        int i=0;
+        int j=n-1;
 
-        int n = s.length();
-        int i = 0;
-        int j = n - 1;
-
-        while (i < j) {
-            if (uset.count(s[i]) && uset.count(s[j])) {
-                swap(s[i], s[j]);
-                i++;
-                j--;
-            } else {
-                if (!uset.count(s[i])) i++;
-                if (!uset.count(s[j])) j--;
+        while(i<=j){
+            if(uset.count(s[i]) && uset.count(s[j])){
+               swap(s[i],s[j]);i++;j--;
+            }else{
+                if(!uset.count(s[i]) && uset.count(s[j])){
+                    i++;
+                }else{
+                    j--;
+                }
             }
         }
         return s;
     }
 };
+
+
+// class Solution {
+// public:
+//     string reverseVowels(string s) {
+//         unordered_set<char> uset;
+//         uset.insert('a');uset.insert('e');uset.insert('i');uset.insert('o');
+//         uset.insert('u');uset.insert('A');uset.insert('E');uset.insert('I');
+//         uset.insert('O');uset.insert('U');
+
+//         int n = s.length();
+//         int i = 0;
+//         int j = n - 1;
+
+//         while (i < j) {
+//             if (uset.count(s[i]) && uset.count(s[j])) {
+//                 swap(s[i], s[j]);
+//                 i++;
+//                 j--;
+//             } else {
+//                 if (!uset.count(s[i])) i++;
+//                 if (!uset.count(s[j])) j--;
+//             }
+//         }
+//         return s;
+//     }
+// };
