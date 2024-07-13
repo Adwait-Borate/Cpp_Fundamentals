3cf36f4580a9f353aef6f173e5d96136ade63a6e
Adwait Prashant Borate <136005137+Adwait-Borate@users.noreply.github.com>
2024-07-13 19:49:48 +0530

    Time: 9 ms (36.61%), Space: 10.5 MB (7.04%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -2,21 +2,30 @@ class Solution {
 public:
     string reverseVowels(string s) {
         unordered_set<char> uset;
-            uset.insert('a');uset.insert('e');uset.insert('i');uset.insert('o');
-        uset.insert('u');uset.insert('A');uset.insert('E');uset.insert('I');
-        uset.insert('O');uset.insert('U');
-        
-        int n=s.length();
-        int i=0;
-        int j=n-1;
+        uset.insert('a');
+        uset.insert('e');
+        uset.insert('i');
+        uset.insert('o');
+        uset.insert('u');
+        uset.insert('A');
+        uset.insert('E');
+        uset.insert('I');
+        uset.insert('O');
+        uset.insert('U');
 
-        while(i<=j){
-            if(uset.count(s[i]) && uset.count(s[j])){
-               swap(s[i],s[j]);i++;j--;
-            }else{
-                if(!uset.count(s[i]) && uset.count(s[j])){
+        int n = s.length();
+        int i = 0;
+        int j = n - 1;
+
+        while (i <= j) {
+            if (uset.count(s[i]) && uset.count(s[j])) {
+                swap(s[i], s[j]);
+                i++;
+                j--;
+            } else {
+                if (!uset.count(s[i]) && uset.count(s[j])) {
                     i++;
-                }else{
+                } else {
                     j--;
                 }
             }
@@ -24,30 +33,3 @@ public:
         return s;
     }
 };
-
-
-// class Solution {
-// public:
-//     string reverseVowels(string s) {
-//         unordered_set<char> uset;
-//         uset.insert('a');uset.insert('e');uset.insert('i');uset.insert('o');
-//         uset.insert('u');uset.insert('A');uset.insert('E');uset.insert('I');
-//         uset.insert('O');uset.insert('U');
-
-//         int n = s.length();
-//         int i = 0;
-//         int j = n - 1;
-
-//         while (i < j) {
-//             if (uset.count(s[i]) && uset.count(s[j])) {
-//                 swap(s[i], s[j]);
-//                 i++;
-//                 j--;
-//             } else {
-//                 if (!uset.count(s[i])) i++;
-//                 if (!uset.count(s[j])) j--;
-//             }
-//         }
-//         return s;
-//     }
-// };
