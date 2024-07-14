89133267eb136d39341e1ecbc085fd203c6654cd
Adwait Prashant Borate <136005137+Adwait-Borate@users.noreply.github.com>
2024-07-14 20:52:28 +0530

    Time: 119 ms (60.37%), Space: 81.2 MB (23.1%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,38 @@
+// class Solution {
+// public:
+//     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+//         int n=nums.size();
+//         int j;int len=0;
+//         for(int i=0;i<n;i++){
+//             j=i+1;
+//             while(j<n){
+//                 if(nums[i]==nums[j]){
+//                     len=j-i;
+//                     if(len<=k){
+//                         return true;
+//                     }
+//                 }
+//                 j++;
+//             }
+//         }
+//         return false;
+//     }
+// };   //GIVING TLE
+
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(std::vector<int>& nums, int k) {
+        std::unordered_map<int, int>umap;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (umap.find(nums[i]) != umap.end()) {
+                if (i - umap[nums[i]] <= k) {
+                    return true; 
+                }
+            }
+            umap[nums[i]] = i;
+        }
+        return false; 
+    }
+};
