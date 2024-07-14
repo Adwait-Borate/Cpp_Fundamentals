0ad7cbab242dd8341626769553b42278b80ce468
Adwait Prashant Borate <136005137+Adwait-Borate@users.noreply.github.com>
2024-07-14 13:31:57 +0530

    Time: 74 ms (50.68%), Space: 86 MB (9.18%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.cpp
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -6,10 +6,10 @@ public:
     bool lemonadeChange(vector<int>& bills) {
         int five = 0, ten = 0;
         
-        for (auto& bill : bills) {
-            if (bill == 5) {
+        for (auto& i : bills) {
+            if (i == 5) {
                 five++;
-            } else if (bill == 10) {
+            } else if (i == 10) {
                 if (five == 0) return false;
                 five--;
                 ten++;
