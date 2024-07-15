1676259e4e402c64da742afa9d6b7d643460eb8a
Adwait Prashant Borate <136005137+Adwait-Borate@users.noreply.github.com>
2024-07-15 09:55:08 +0530

    Time: 191 ms (12.97%), Space: 90.3 MB (12.55%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.cpp
+++ 0937-online-stock-span/0937-online-stock-span.cpp
@@ -1,10 +1,6 @@
 class StockSpanner {
 public:
-stack<pair<int,int>> st;
-    StockSpanner() {
-        
-    }
-    
+   stack<pair<int,int>> st;
     int next(int price) {
         int span=1;
 
@@ -17,9 +13,3 @@ stack<pair<int,int>> st;
         return span;
     }
 };
-
-/**
- * Your StockSpanner object will be instantiated and called as such:
- * StockSpanner* obj = new StockSpanner();
- * int param_1 = obj->next(price);
- */
