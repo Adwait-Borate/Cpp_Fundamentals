30f323ccc88c97c9aee066ece6ad029144ae8076
Adwait Prashant Borate <136005137+Adwait-Borate@users.noreply.github.com>
2024-07-19 20:12:36 +0530

    Time: 234 ms (91.72%), Space: 93.6 MB (29.13%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,57 +1,24 @@
-// class Solution {
-// public:
-//     int findMinArrowShots(vector<vector<int>>& points) {
-//          int n = points.size();
-//         int i = 0;
-//         int j = 1;
-//         int cnt = 0;
-//         sort(points.begin(), points.end());
-//         while (j < n) {
-//             vector<int> currentInterval = points[i];
-//             vector<int> nextInterval = points[j];
-
-//             int currentStart = currentInterval[0];
-//             int currentEnd = currentInterval[1];
-
-//             int nextStart = nextInterval[0];
-//             int nextEnd = nextInterval[1];
-
-//             if (currentEnd <= nextStart) {
-//                 i = j;
-//                 j++;
-//             } else if (currentEnd <= nextEnd) {
-//                 j++;
-//                 cnt++;
-//             } else if (currentEnd > nextEnd) {
-//                 i = j;
-//                 j++;
-//                 cnt++;
-//             }
-//         }
-//         return cnt;
-//     }
-// };
-
 class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
         int n = points.size();
-        if (n == 0) return 0;
-
-       
-        sort(points.begin(), points.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[1] < b[1];
-        });
-
-        int i = 0;
+        sort(points.begin(), points.end());
+        vector<int> prev = points[0];
         int cnt = 1;
-        for (int j = 1; j < n; j++) {
-            int currentEnd = points[i][1];
-            int nextStart = points[j][0];
 
-            if (currentEnd < nextStart) {
-                i = j;
+        for(int i=1;i<n;i++){
+            int currentStart=points[i][0];
+            int currentEnd=points[i][1];
+
+            int previousStart=prev[0];
+            int previousEnd=prev[1];
+
+            if(currentStart>previousEnd){ 
                 cnt++;
+                prev=points[i];
+            }else{ //OVERLAP 
+                prev[0]=max(previousStart,currentStart);
+                prev[1]=min(previousEnd,currentEnd);
             }
         }
         return cnt;
