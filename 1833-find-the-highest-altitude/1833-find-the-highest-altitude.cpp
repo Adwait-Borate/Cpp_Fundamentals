17d413d9d6c1eac88d462a3e86bb307173e29739
Adwait Prashant Borate <136005137+Adwait-Borate@users.noreply.github.com>
2024-07-22 20:09:47 +0530

    Time: 5 ms (14.11%), Space: 10.3 MB (5.84%) - LeetHub

---
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cpp
@@ -0,0 +1,35 @@
+// class Solution {
+// public:
+//     int largestAltitude(vector<int>& gain) {
+       
+//         int n=gain.size();
+//          vector<int>diff(n);
+//         diff[0]=0;
+//         diff[1]=gain[0];
+
+//         for(int i=2;i<n;i++){
+//           diff[i]=diff[i-1]+gain[i-1];
+//         }
+//         int maxi=*max_element(diff.begin(),diff.end());
+
+//         return maxi; 
+//     }
+// };
+
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int n = gain.size();
+        vector<int> diff(n + 1, 0); 
+
+        diff[0] = 0;
+        diff[1] = gain[0];
+
+        for (int i = 2; i <= n; i++) {
+            diff[i] = diff[i - 1] + gain[i - 1];
+        }
+
+        int maxi = *max_element(diff.begin(), diff.end());
+        return maxi;
+    }
+};
