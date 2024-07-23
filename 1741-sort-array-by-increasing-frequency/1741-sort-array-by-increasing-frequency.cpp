b3125cdfef32e92801c4ef6b60a3da99f79dcf75
Adwait Prashant Borate <136005137+Adwait-Borate@users.noreply.github.com>
2024-07-23 11:41:30 +0530

    Time: 11 ms (17.92%), Space: 14.8 MB (55.33%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
 
-        unordered_map<int,int>umap;
-        for(auto &i:nums){
+        unordered_map<int, int> umap;
+        for (auto& i : nums) {
             umap[i]++;
         }
 
-        sort(nums.begin(),nums.end(),[&](int n1,int n2){
-            if(umap[n1]==umap[n2]){
-               
-                return n1>n2;
-            }else{
-                return umap[n1]<umap[n2];
+        sort(nums.begin(), nums.end(), [&](int n1, int n2) {
+            if (umap[n1] == umap[n2]) {
+                return n1 > n2;
+            } else {
+                return umap[n1] < umap[n2];
             }
         });
 
