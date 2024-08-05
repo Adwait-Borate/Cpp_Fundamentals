48050deac98c60e61a434568ac43ea1fdbf95399
Adwait Prashant Borate <136005137+Adwait-Borate@users.noreply.github.com>
2024-08-05 22:35:25 +0530

    Time: 284 ms (67.06%), Space: 0B (100%) - LeetHub

---
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -0,0 +1,10 @@
+-- # Write your MySQL query statement below
+-- select score,
+-- DENSE_RANK() OVER(ORDER BY score desc) as rank
+-- from Scores
+-- ORDER BY score desc;
+
+SELECT score,
+       DENSE_RANK() OVER (ORDER BY score DESC) AS 'rank'
+FROM Scores;
+
