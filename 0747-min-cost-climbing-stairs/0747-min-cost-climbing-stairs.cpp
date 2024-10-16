85e8324b533cf07f1e5438103952952ae6bfd04c
Adwait Prashant Borate <136005137+Adwait-Borate@users.noreply.github.com>
2024-10-16 19:47:53 +0530

    Time: 7 ms (32.35%), Space: 16.7 MB (53.29%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,21 +1,43 @@
+// class Solution {
+// public:
+//     int dp[1001];
+//     int solve(int idx, vector<int>& cost) {
+//         if (idx >= cost.size()) {
+//             return 0;
+//         }
+//         if (dp[idx] != -1) {
+//             return dp[idx];
+//         }
+//         int step1 = 0, step2 = 0;
+//         step1 = cost[idx] + solve(idx + 1, cost);
+//         step2 = cost[idx] + solve(idx + 2, cost); 
+
+//         return dp[idx] = min(step1, step2);
+//     }
+//     int minCostClimbingStairs(vector<int>& cost) {
+//         memset(dp, -1, sizeof(dp));
+//         return min(solve(0, cost), solve(1, cost));
+//     }
+// };
+
+
 class Solution {
 public:
-    int dp[1001];
-    int solve(int idx, vector<int>& cost) {
-        if (idx >= cost.size()) {
-            return 0;
-        }
-        if (dp[idx] != -1) {
-            return dp[idx];
-        }
-        int step1 = 0, step2 = 0;
-        step1 = cost[idx] + solve(idx + 1, cost);
-        step2 = cost[idx] + solve(idx + 2, cost);
-
-        return dp[idx] = min(step1, step2);
-    }
     int minCostClimbingStairs(vector<int>& cost) {
-        memset(dp, -1, sizeof(dp));
-        return min(solve(0, cost), solve(1, cost));
+        int n = cost.size();
+        
+
+        if (n == 1) return cost[0];
+        if (n == 2) return min(cost[0], cost[1]);
+        
+        vector<int> dp(n);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for (int i = 2; i < n; ++i) {
+            dp[i] = cost[i] + min(dp[i - 1], dp[i - 2]);
+        }
+
+        return min(dp[n - 1], dp[n - 2]);
     }
 };
