288ac94947ccf730b0b2e377f1546f4597db3306
Adwait Prashant Borate <136005137+Adwait-Borate@users.noreply.github.com>
2024-10-16 18:26:11 +0530

    Time: 0 ms (100%), Space: 7.4 MB (88.89%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        if (n == 0 || n == 1) return 1;
-
-        vector<int> dp(n+1, -1); 
-        
-        dp[0] = 1;
-        dp[1] = 1; 
-
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+    int dp[46];
+    int solve(int n) {
+        if (n == 0) {
+            return 1;
         }
 
-        return dp[n]; 
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+        int step1 = 0, step2 = 0;
+
+        if (n - 1 >= 0) {
+            step1 = solve(n - 1);
+        }
+
+        if (n - 2 >= 0) {
+            step2 = solve(n - 2);
+        }
+
+        return dp[n] = step1 + step2;
+    }
+
+    int climbStairs(int n) {
+        memset(dp, -1, sizeof(dp));
+        return solve(n);
     }
 };
