07f893a0c8c13b5d03376caa3a5cbfbcc24e9e63
Adwait Prashant Borate <136005137+Adwait-Borate@users.noreply.github.com>
2024-10-16 19:50:30 +0530

    Time: 3 ms (35.61%), Space: 7.8 MB (67.49%) - LeetHub

---
+++ 1769-get-maximum-in-generated-array/1769-get-maximum-in-generated-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int getMaximumGenerated(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+
+        vector<int> nums(n + 1);
+        nums[0] = 0;
+        nums[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            if (i % 2 == 0) {
+                nums[i] = nums[i / 2];
+            } else {
+                nums[i] = nums[i / 2] + nums[i / 2 + 1];
+            }
+        }
+
+        int maxVal = 0;
+        for (int i = 0; i <= n; ++i) {
+            maxVal = max(maxVal, nums[i]);
+        }
+
+        return maxVal;
+    }
+};
