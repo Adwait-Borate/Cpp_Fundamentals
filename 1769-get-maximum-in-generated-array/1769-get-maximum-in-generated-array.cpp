209f3185d05904ecf089f3d40e45c07994a64ee9
Adwait Prashant Borate <136005137+Adwait-Borate@users.noreply.github.com>
2024-10-17 10:18:44 +0530

    Time: 0 ms (100%), Space: 7.8 MB (34.86%) - LeetHub

--- 1769-get-maximum-in-generated-array/1769-get-maximum-in-generated-array.cpp
+++ 1769-get-maximum-in-generated-array/1769-get-maximum-in-generated-array.cpp
@@ -1,16 +1,43 @@
+// class Solution {
+// public:
+//     int getMaximumGenerated(int n) {
+//         if (n == 0)
+//             return 0;
+//         if (n == 1)
+//             return 1;
+
+//         vector<int> nums(n + 1);
+//         nums[0] = 0;
+//         nums[1] = 1;
+
+//         for (int i = 2; i <= n; ++i) {
+//             if (i % 2 == 0) {
+//                 nums[i] = nums[i / 2];
+//             } else {
+//                 nums[i] = nums[i / 2] + nums[i / 2 + 1];
+//             }
+//         }
+
+//         int maxVal = 0;
+//         for (int i = 0; i <= n; ++i) {
+//             maxVal = max(maxVal, nums[i]);
+//         }
+
+//         return maxVal;
+//     }
+// };
+
+
 class Solution {
 public:
     int getMaximumGenerated(int n) {
-        if (n == 0)
-            return 0;
-        if (n == 1)
-            return 1;
+        if (n == 0) return 0; 
 
         vector<int> nums(n + 1);
         nums[0] = 0;
         nums[1] = 1;
 
-        for (int i = 2; i <= n; ++i) {
+        for (int i = 2; i <= n; i++) {
             if (i % 2 == 0) {
                 nums[i] = nums[i / 2];
             } else {
@@ -18,11 +45,6 @@ public:
             }
         }
 
-        int maxVal = 0;
-        for (int i = 0; i <= n; ++i) {
-            maxVal = max(maxVal, nums[i]);
-        }
-
-        return maxVal;
+        return *max_element(nums.begin(), nums.end());
     }
 };
